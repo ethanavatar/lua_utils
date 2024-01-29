229f77be690d9f07091db91d2200e75f629f08bf
Ethan Evans <ethanalexevans@gmail.com>
2024-01-28 22:03:43 -0800

    run stylua

--- utils.lua
+++ utils.lua
@@ -1,17 +1,17 @@
-require "io"
-local module_name = "utils"
-local module_path =  package.searchpath(module_name, package.path)
+require('io')
+local module_name = 'utils'
+local module_path = package.searchpath(module_name, package.path)
 
 if not module_path then
-    print("Could not access location of module: " .. module_name)
+    print('Could not access location of module: ' .. module_name)
     return {}
 end
 
 -- module_path without the module name
 local module_folder = module_path:sub(1, module_path:find(module_name) - 2)
 
-if module_folder == "." then
-    module_folder = ""
+if module_folder == '.' then
+    module_folder = ''
 end
 
 local path_separator = package.config:sub(1, 1)
@@ -19,9 +19,9 @@ local path_separator = package.config:sub(1, 1)
 local M = {}
 
 local function join_paths(path1, path2)
-    if path1 == "" then
+    if path1 == '' then
         return path2
-    elseif path2 == "" then
+    elseif path2 == '' then
         return path1
     end
 
@@ -32,12 +32,16 @@ end
 -- Description: list all files in a directory
 -- Parameters:
 --  dir: directory to list the contents of
---  recursive: whether to list recursively 
+--  recursive: whether to list recursively
 -- Returns:
 -- table of file paths as strings
 local function list_dir(dir, recursive)
-    local recursive_flag = recursive and "-r" or ""
-    local cmd = join_paths(module_folder, 'list_dir.exe') .. " " .. dir .. " " .. recursive_flag
+    local recursive_flag = recursive and '-r' or ''
+    local cmd = join_paths(module_folder, 'list_dir.exe')
+        .. ' '
+        .. dir
+        .. ' '
+        .. recursive_flag
     print('Running: ' .. cmd)
     local handle = io.popen(cmd)
 
